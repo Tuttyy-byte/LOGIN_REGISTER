48756c368fc4dd2581600e507de81aa5973f94e4
Tuttyy-byte <aruu0213@gmail.com>
2026-05-06 03:10:56 +0800

    add new to branch 1

--- db_connection.php
+++ db_connection.php
@@ -1,18 +1,14 @@
 <?php
-// Database configuration
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "itfest_sql_competition";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Set charset to UTF-8
 mysqli_set_charset($conn, "utf8");
 ?>
